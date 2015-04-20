852504010db87b7008c55f562b618466397a40f1
pst <399817368@qq.com>
2015-04-20 11:28:35 +0800

    初始化

---
+++ src/main/java/com/zybr/admin/Test.java
@@ -0,0 +1,13 @@
+package com.zybr.admin;
+
+/**
+ * Created by pst on 15-4-20.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("=================");
+
+    }
+
+}
